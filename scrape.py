df13c82291d8affca34fd063e1b6d087e61f124f
Joshua Springer <jspri17@gmail.com>
2022-09-23 18:01:26 +0000

    Added stuff to the scraper and started on database.

--- scrape.py
+++ scrape.py
@@ -9,12 +9,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import pandas
+import csv
+
+from io import StringIO
+import lxml.etree
+import io
+from PIL import Image
 import time
 
 def fa_ord(word):
-   
+
+    print(f"looking up '{word}'")
+
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get(f"https://bin.arnastofnun.is/leit/beygingarmynd/{word}")
 
@@ -36,23 +45,20 @@ def fa_ord(word):
         word = possible_words[index].text.split(" ")[0]
         possible_words[index].find_element(By.XPATH, "./b/a").click()
 
-    current_url = driver.current_url
-    WebDriverWait(driver, 5).until_not(EC.url_changes(current_url))
-    #time.sleep(1)
+        time.sleep(2)
+        #print("Waiting for webpage to change...")
+        #current_url = driver.current_url
+        #print(dir(EC))
+        #WebDriverWait(driver, 5).until_not(EC.url_matches(current_url))
+        #print("Webpage changed - proceeding...")
 
-    print(f"searching for {word}")
-    #search_string  = f"//*[contains(text(), '{word} ')]"
-    search_string  = f"//*[contains(text(), '{word}')]"
-
-    classification = driver.find_element(By.XPATH, search_string)
-
-    print(f"classification: {classification}")
-
-    #time.sleep(10)
+    element = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div[1]/div[2]/div/div[1]/div/div[1]")
 
     part_of_speech = None
     gender = None
     flaschard_version = None
+    
+    classification = element.text
 
     if( "nafnorð" in classification ):
         
@@ -64,11 +70,64 @@ def fa_ord(word):
         flashcard_version = flashcard_version.find_element(By.XPATH, "../../../tbody/tr[1]/td[3]/span").text
 
     print(f"{flashcard_version} ({word}): {part_of_speech}, {gender}")
+    print()
 
-#fa_ord("hádegismat")
-fa_ord("hundur")
-fa_ord("banani")
-fa_ord("kaka")
-fa_ord("ostur")
-fa_ord("safi")
-fa_ord("fiskur")
+    return flashcard_version
+
+def get_english(word):
+
+    print(f"translating to english: '{word}'")
+
+    options = Options()
+    #options.add_argument("--headless")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    driver.get(f"https://www.wordreference.com/isen/{word}")
+
+    print("loaded webpage...")
+
+    input()
+
+    #search_string  = f"//*[contains(text(), 'Aðalþýðingar')]"
+    #search_string  = f"//*[contains(text(), 'enska')]"
+    search_string  = f"//*[normalize-space(text())='enska']"
+    english_version = driver.find_element(By.XPATH, search_string)
+
+    #for version in english_version:
+    #    print(version.text)
+
+    english_version = english_version.find_element(By.XPATH, "../../../tr[3]/td/strong").text
+
+    print(f"definition: {english_version}")
+
+    return english_version
+
+def process( row ):
+    front = fa_ord(row["word"])
+    back  = get_english(row["front"])
+    
+    return front, back
+
+get_english("banani")
+
+columns = ["initial", "front", "back"]
+
+database = None
+try:
+    database = pandas.read_csv("database.csv")
+except:
+    database = pandas.DataFrame(columns=columns)
+
+new_words = pandas.read_csv("new_words.csv")
+#new_words["front"] = new_words.apply( lambda row: fa_ord(row.word),      axis=1 )
+new_words["back"]  = new_words.apply( lambda row: get_english(row.word), axis=1 )
+#new_words[["front", "back"]] = new_words.apply( lambda row: process( row ), axis=1)
+
+database = pandas.merge(database, new_words, on="front", how="left")
+database = database[columns]
+
+database["initial"] = new_words["word"]
+database["front"] = new_words["front"]
+print(database)
+
+database.to_csv("database.csv", index=False, quoting=csv.QUOTE_ALL)
